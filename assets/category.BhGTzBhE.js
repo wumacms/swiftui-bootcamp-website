a4632396502157da8338808d0a85f747e6c3c959
BlockCMS <noreply@blockcms.com>
2025-11-05 11:16:43 +0800

    Deploy site: SwiftUI训练营 at 2025-11-05T11:16:43.733

---
+++ assets/category.BhGTzBhE.js
@@ -0,0 +1,1 @@
+import{K as r}from"./index.CYdA3MFZ.js";const a={getCategories:e=>r.get(`/sites/${e}/categories`),createCategory:(e,t)=>r.post(`/sites/${e}/categories`,t),updateCategory:(e,t,s)=>r.put(`/sites/${e}/categories/${t}`,s),deleteCategory:(e,t)=>r.delete(`/sites/${e}/categories/${t}`)};export{a as c};
